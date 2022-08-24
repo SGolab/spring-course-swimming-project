4b8b2d8372e69d2f67e8df3cc8f685ab99b5e812
SGolab <szym.golab@gmail.com>
2022-08-24 21:10:55 +0200

    Added @ToString(exclude = "lessons") to prevent StackOverflowException

--- src/main/java/com/example/sgswimming/model/Swimmer.java
+++ src/main/java/com/example/sgswimming/model/Swimmer.java
@@ -1,9 +1,6 @@
 package com.example.sgswimming.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -13,6 +10,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "lessons")
 @Entity
 public class Swimmer {
 
