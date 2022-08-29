2d4da5956fb8cecd92f2aa0919a7ec4edae62cfe
SGolab <szym.golab@gmail.com>
2022-08-29 16:31:34 +0200

    Added maven dependency module to jackson to process LocalDateTime. Created Json LocalDateTime config class.

---
+++ src/main/java/com/example/sgswimming/config/JsonDateMappingConfig.java
@@ -0,0 +1,25 @@
+package com.example.sgswimming.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class JsonDateMappingConfig {
+
+    public static final String DATE_FORMAT = "dd.MM.yyyy";
+    public static final String DATE_TIME_FORMAT = "HH:mm dd.MM.yyyy";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(DATE_TIME_FORMAT);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)));
+        };
+    }
+}
