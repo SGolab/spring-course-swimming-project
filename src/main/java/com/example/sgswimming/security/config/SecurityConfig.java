3767b145f284f2ce2d5934f059f950b20fd2be14
SGolab <szym.golab@gmail.com>
2022-09-12 21:24:00 +0200

    Added basic security configuration and jpa User Details implementation.

---
+++ src/main/java/com/example/sgswimming/security/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.sgswimming.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    private final UserDetailsService userDetailsService;
+
+    AuthenticationManager authenticationManager;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService);
+        authenticationManager = authenticationManagerBuilder.build();
+
+        http
+                .csrf().ignoringAntMatchers("/h2-console/**", "/api/**").and()
+                .cors().disable()
+                .authorizeHttpRequests().antMatchers("/h2-console/**", "/", "/webjars/**", "/login", "/resources/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .authenticationManager(authenticationManager)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    // needed for use with Spring Data JPA SPeL
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
+    }
+}
